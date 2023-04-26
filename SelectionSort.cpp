ce31908276aeef130a42a12490d37b27f4b0c190
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-04-26 12:23:07 -0700

    updated manual swap to std::swap

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -10,26 +10,22 @@
 *****************************************************************************/
 
 #include <vector>
+#include <utility>  //std::swap()
 
 template <class T>
 void SelectionSort(std::vector<T>& array) {
-    // i is current index of the sorted array
     for (int i = 0; i < array.size()-1; ++i) {
 
         int min_index = i;
         
-        // j is the current index of the unsorted array
         for (int j = i+1; j < array.size(); ++j) {
             if (array[j] < array[min_index]) {
                 min_index = j;
             }
         }
 
-        // swap the next smallest element if found this iteration
         if (i != min_index) {
-            T temp = array[i];
-            array[i] = array[min_index];
-            array[min_index] = temp;
+            std::swap(array[i], array[min_index]);
         }
     }
 }
